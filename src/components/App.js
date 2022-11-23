d965938241c96baff6f0855bb5abebc05ae47b71
Jaya <117105143+Jayashree-74@users.noreply.github.com>
2022-11-23 19:54:31 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React, { Component, useState } from "react";
 import '../styles/App.css';
-
+import {Heading} from './Heading';
 const App = () => {
   return (
     <div id="main">
-      < />
+      <Heading />
       < />
       < />
       < />
